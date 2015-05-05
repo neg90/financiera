dc5c71cf2c8bdc8971e20c0e997b1445da9d7d93
Neg <neg90.ng@gmail.com>
2015-05-05 09:18:41 -0300

    limpieza de estructura

--- controlador/cargaTwig.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	Twig_Autoloader::register();
-	$loader = new Twig_Loader_Filesystem('../vista');
-	$twig = new Twig_Environment($loader, array('debug' => 'false'));//'cache' => '../cache',')); 
-
-?>
--- controlador/publico.php
+++ controlador/publico.php
@@ -2,19 +2,18 @@
 
 require_once '../vendor/autoload.php';
 
-
 class ControladorPublico {
-	static function Principal(){
+
+	public function Principal(){
 
 		Twig_Autoloader::register();
 		$loader = new Twig_Loader_Filesystem('../vista');
 	    $twig = new Twig_Environment($loader, array('debug' => 'false'));//'cache' => '../cache',')); 
+		
+
 
 		$template = $twig->loadTemplate('home.html.twig');
 		echo $template->render(array('errorLoginVista'=>$errorLoginVista));
-	  	
-			
-
 		
 	}		
 }
