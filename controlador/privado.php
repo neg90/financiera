326473fcfb6c898b8b96c0225492ee45495d83a1
Neg.- <neg90.ng@gmail.com>
2015-05-09 06:32:19 -0300

    creado cargar noticia

--- controlador/privado.php
+++ controlador/privado.php
@@ -1,19 +1,19 @@
 <?php
 
-require_once '../Twig/Autoloader.php';	
-	
+require_once '../vendor/twig/twig/lib/Twig/Autoloader.php';
 
-try {
+
  	Twig_Autoloader::register();
   	$loader = new Twig_Loader_Filesystem('../vista');
-  	$twig = new Twig_Environment($loader, array(//'cache' => '../cache','
-    'debug' => 'false')); 
+  	$twig = new Twig_Environment($loader, array('cache' => '../cache','debug' => 'false')); 
 
   	session_start();
 	if (!isset($_SESSION['user'])){
 		//No deberias estar aca pero buen intento campeon
 	}else{
+		$template = $twig->loadTemplate('noticia.html.twig');
+		echo $template->render(array());
 		//Enrrutador
-		
+
 	}
 ?>
