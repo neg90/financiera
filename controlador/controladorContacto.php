8dba0fd3388915bf37c3d8cfc266d69087d7bc58
Neg.- <neg90.ng@gmail.com>
2015-05-26 01:16:01 -0300

    contacto conectado al controlador

--- controlador/controladorContacto.php
+++ controlador/controladorContacto.php
@@ -6,8 +6,12 @@
 class controladorContacto {
 
 	static function modificar(){
-		
-		}
+		Twig_Autoloader::register();
+	  	$loader = new Twig_Loader_Filesystem('../vista');
+	  	$twig = new Twig_Environment($loader, array('cache' => '../cache','debug' => 'false')); 
+		$template = $twig->loadTemplate('contacto.html.twig');
+		echo $template->render(array('error'=>0));
+	}
 
 }
 
