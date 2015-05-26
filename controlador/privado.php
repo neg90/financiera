28b05d3976be78a4e798f6ab5a461d94b834e137
Neg.- <neg90.ng@gmail.com>
2015-05-26 01:24:17 -0300

    misdatos feltado

--- controlador/privado.php
+++ controlador/privado.php
@@ -1,22 +1,20 @@
 <?php
 
 require_once '../vendor/twig/twig/lib/Twig/Autoloader.php';
-require_once 'controladorMisDatos.php';
+
 require_once 'controladorNoticias.php';
 require_once 'controladorContacto.php';
 
 
   	session_start();
 	if (!isset($_SESSION['user'])){
-		ControladorDeAcceso::principal();
+		//si quiere volver para atras aca avisa
 		//No deberias estar aca pero buen intento campeon de la luz.
 	}else{
 		$controlador=htmlEntities(@$_GET['c']); 
 		$accion=htmlEntities(@$_GET['a']); 
 		if ((!empty($controlador)) and (!empty($accion))){
-			if (($controlador=='misdatos') and ($accion=='modificar')) {
-				controladorMisDatos::modificar();
-			}elseif (($controlador=='correo') and ($accion=='enviar')) {
+			if (($controlador=='correo') and ($accion=='enviar')) {
 				controladorCorreo::enviar();
 			}elseif (($controlador=='noticias') and ($accion=='modificar')){
 				controladorNoticias::modificar();
